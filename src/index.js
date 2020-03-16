405433c58f3908b8b8621ff376acd7e1600359f2
Natalia Derbeneva <nat_ff@mail.ru>
2020-03-16 13:32:28 +0300

    init: start 'brackets' task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+   
+    let arr = new Array();
+    for(let i=0; i<str.length; i++){
+        for(let j=0; j<bracketsConfig.length; j++){
+            if(str[i] == bracketsConfig[j][0]){
+              if(bracketsConfig[j][0]==bracketsConfig[j][1] && arr[arr.length-1]==j){
+                arr.pop();
+                break;  
+              }
+              arr.push(j);
+              break;
+            } 
+            
+            if(str[i] == bracketsConfig[j][1]){
+                if(arr[arr.length-1] == j) {
+                  arr.pop();
+                  break;
+                } else return false;  
+            }
+        }
+    }
+  
+    if(arr.length == 0) return true;  
+    else return false;
 }
